5e8a4aa695c9800f0eaf074e405f9d1ce638123b
hhlee <hhlee@b2en.com>
2023-12-26 17:06:40 +0900

    spring boot sample code

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,14 +10,22 @@ group = "com.hhlee"
 version = "0.0.1-SNAPSHOT"
 
 java.sourceCompatibility = JavaVersion.VERSION_17
-//val springBootVersion : String by extra("3.1.6")
+val springBootVersion : String by extra("3.1.6")
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("commons-io:commons-io:2.15.1")
+    implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-tomcat:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-mail:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-websocket:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-security:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-aop:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-cache:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-webflux:$springBootVersion")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
