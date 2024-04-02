6fbfb46c9ff52e0006487e056a4511b760da9709
hhlee <hhlee@b2en.com>
2024-04-02 12:18:51 +0900

    main 변경

--- src/main/java/com/hhlee/StartApplication.java
+++ src/main/java/com/hhlee/StartApplication.java
@@ -8,16 +8,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
-@SpringBootApplication(
-        exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class},
-        scanBasePackages = "com.hhlee"
-)
-@PropertySources({
-        @PropertySource(value = "classpath:config/application.properties", ignoreResourceNotFound = true)
-})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class}, scanBasePackages = "com.hhlee")
+@PropertySources({@PropertySource(value = "classpath:config/application.properties", ignoreResourceNotFound = true)})
 public class StartApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(StartApplication.class, args);
+        System.out.println("이렇게 추가했어");
+        
     }
 }
